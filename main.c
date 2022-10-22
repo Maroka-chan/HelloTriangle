1d1746751740c890f89ffda4b607bb84e1e0d4eb
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2022-10-22 21:33:11 +0200

    Implement chooseSwapPresentMode and change chooseSwapSurfaceFormat parameter

--- main.c
+++ main.c
@@ -304,15 +304,26 @@ SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) {
 }
 
 
-VkSurfaceFormatKHR chooseSwapSurfaceFormat(const VkSurfaceFormatKHR** availableFormats) {
-  foreach(availableFormat, *availableFormats) {
+VkSurfaceFormatKHR chooseSwapSurfaceFormat(const VkSurfaceFormatKHR* availableFormats) {
+  foreach(availableFormat, availableFormats) {
     if (availableFormat->format == VK_FORMAT_B8G8R8A8_SRGB &&
         availableFormat->colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
       return *availableFormat;
     }
   }
 
-  return *availableFormats[0];
+  return availableFormats[0];
+}
+
+VkPresentModeKHR chooseSwapPresentMode(const VkPresentModeKHR* availablePresentModes) {
+  foreach(availablePresentMode, availablePresentModes) {
+    if (*availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+      return *availablePresentMode;
+    }
+  }
+
+
+  return VK_PRESENT_MODE_FIFO_KHR;
 }
 
 
