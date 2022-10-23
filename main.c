dd5792fbbb8ed41c65ea7d0dd3d4ba6acfd65e1a
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2022-10-23 16:37:23 +0200

    Retrieve swap chain images

--- main.c
+++ main.c
@@ -62,6 +62,16 @@ VkQueue presentQueue;
 // Handle to the swap chain
 VkSwapchainKHR swapChain;
 
+// Handle to the swap chain images
+// The images will be automatically cleaned up once
+// the swap chain has been destroyed
+VkImage* swapChainImages;
+
+// Handle to the swap chain image format
+VkFormat swapChainImageFormat;
+// Handle to the swap chain extent
+VkExtent2D swapChainExtent;
+
 
 // Handle to the Debug callback
 VkDebugUtilsMessengerEXT debugMessenger;
@@ -606,6 +616,13 @@ void createSwapChain() {
     error("Failed to create swap chain!\n");
     exit(EXIT_FAILURE);
   }
+
+  vkGetSwapchainImagesKHR(device, swapChain, &imageCount, NULL);
+  swapChainImages = malloc(imageCount * sizeof(typeof(*swapChainImages)));
+  vkGetSwapchainImagesKHR(device, swapChain, &imageCount, swapChainImages);
+
+  swapChainImageFormat = surfaceFormat.format;
+  swapChainExtent = extent;
 }
 
 void initVulkan() {
